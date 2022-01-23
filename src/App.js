d904b24ae637f942d097a05c3c2855eb6c5e5dee
Lucas Dreher <lucasdreher@gmail.com>
2022-01-23 23:19:06 +0100

    Preparing The Project For The Next Steps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
@@ -12,10 +13,13 @@ function App() {
 		setIsLoading(true);
 		setError(null);
 		try {
-			const response = await fetch('https://swapi.dev/api/films/');
+			const response = await fetch(
+				'https://react-http-88f71-default-rtdb.europe-west1.firebasedatabase.app/movies.json'
+			);
 			if (!response.ok) {
 				throw new Error('Something went wrong!');
 			}
+
 			const data = await response.json();
 
 			const transformedMovies = data.results.map((movieData) => {
@@ -40,20 +44,29 @@ function App() {
 		[ fetchMoviesHandler ]
 	);
 
-	let content = <p>Found no movies</p>;
+	function addMovieHandler(movie) {
+		console.log(movie);
+	}
+
+	let content = <p>Found no movies.</p>;
 
 	if (movies.length > 0) {
 		content = <MoviesList movies={movies} />;
 	}
+
 	if (error) {
 		content = <p>{error}</p>;
 	}
+
 	if (isLoading) {
 		content = <p>Loading...</p>;
 	}
 
 	return (
 		<React.Fragment>
+			<section>
+				<AddMovie onAddMovie={addMovieHandler} />
+			</section>
 			<section>
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -4,18 +4,13 @@ import Movie from './Movie';
 import classes from './MoviesList.module.css';
 
 const MovieList = (props) => {
-  return (
-    <ul className={classes['movies-list']}>
-      {props.movies.map((movie) => (
-        <Movie
-          key={movie.id}
-          title={movie.title}
-          releaseDate={movie.releaseDate}
-          openingText={movie.openingText}
-        />
-      ))}
-    </ul>
-  );
+	return (
+		<ul className={classes['movies-list']}>
+			{props.movies.map((movie) => (
+				<Movie key={movie.id} title={movie.title} releaseDate={movie.releaseDate} openingText={movie.openingText} />
+			))}
+		</ul>
+	);
 };
 
 export default MovieList;
